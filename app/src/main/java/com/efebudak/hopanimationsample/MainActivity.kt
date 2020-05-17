e1a0a791af12805c765351f4a853f9776590490d
Efe Budak <efebudak1989@gmail.com>
2020-05-17 21:11:03 +0100

    - Update gradle version - Add ValueAnimator sample code

--- app/src/main/java/com/efebudak/hopanimationsample/MainActivity.kt
+++ app/src/main/java/com/efebudak/hopanimationsample/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.efebudak.hopanimationsample
 
+import android.animation.ValueAnimator
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.efebudak.hopper.HopDirection
 import com.efebudak.hopper.Hopper
@@ -9,25 +9,46 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-        val hopper = Hopper.HopperBuilder(textView)
-            .hopDirection(HopDirection.LEFT_TO_RIGHT)
-            .build()
-        hopper.start()
-        //hopper.end()
+    val hopper = Hopper.HopperBuilder(textView)
+      .hopDirection(HopDirection.LEFT_TO_RIGHT)
+      .build()
+    hopper.start()
+    //hopper.end()
 
-        Hopper.HopperBuilder(buttonLogin)
-            .distanceInDp(16f)
-            .duration(750)
-            .times(25)
-            .addHopperFinishedListener {
-                //Do what you want to do
-            }
-            .build()
-            .start()
+    Hopper.HopperBuilder(buttonLogin)
+      .distanceInDp(16f)
+      .duration(750)
+      .times(25)
+      .addHopperFinishedListener {
+        //Do what you want to do
+      }
+      .build()
+      .start()
 
+
+    val animator = ValueAnimator.ofInt(0, 10)
+
+    animator.duration = 10000
+    animator.addUpdateListener { valueAnimator ->
+
+      textViewValueAnimator.text = valueAnimator.animatedValue.toString()
     }
+
+    buttonStartValueAnimation.setOnClickListener {
+      animator.start()
+    }
+
+    buttonEndValueAnimator.setOnClickListener {
+      animator.end()
+    }
+
+    buttonCancelValueAnimator.setOnClickListener {
+      animator.cancel()
+    }
+
+  }
 }
