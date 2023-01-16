b84523111c9af5dd472aa195ae014e98c6d27be6
efebudak <efebudak1989@gmail.com>
2023-01-16 22:54:45 +0000

    upgrade gradle and android gradle plugin versions migrate the MainActivity.kt to the view binding

--- app/src/main/java/com/efebudak/hopanimationsample/MainActivity.kt
+++ app/src/main/java/com/efebudak/hopanimationsample/MainActivity.kt
@@ -3,54 +3,57 @@ package com.efebudak.hopanimationsample
 import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.efebudak.hopanimationsample.databinding.ActivityMainBinding
 import com.efebudak.hopper.HopDirection
 import com.efebudak.hopper.Hopper
 import com.efebudak.hopper.hopToDirection
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    private lateinit var binding: ActivityMainBinding
 
-    val hopper = Hopper.HopperBuilder(textView)
-      .hopDirection(HopDirection.LEFT_TO_RIGHT)
-      .build()
-    hopper.start()
-    //hopper.end()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    Hopper.HopperBuilder(buttonLogin)
-      .distanceInDp(16f)
-      .duration(750)
-      .times(25)
-      .addHopperFinishedListener {
-        //Do what you want to do
-      }
-      .build()
-      .start()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    buttonCancelValueAnimator.hopToDirection(HopDirection.RIGHT_TO_LEFT)
+        val hopper = Hopper.HopperBuilder(binding.textView)
+            .hopDirection(HopDirection.LEFT_TO_RIGHT)
+            .build()
+        hopper.start()
+        //hopper.end()
 
-    val animator = ValueAnimator.ofInt(0, 10)
+        Hopper.HopperBuilder(binding.buttonLogin)
+            .distanceInDp(16f)
+            .duration(750)
+            .times(25)
+            .addHopperFinishedListener {
+                //Do what you want to do
+            }
+            .build()
+            .start()
 
-    animator.duration = 10000
-    animator.addUpdateListener { valueAnimator ->
+        binding.buttonCancelValueAnimator.hopToDirection(HopDirection.RIGHT_TO_LEFT)
 
-      textViewValueAnimator.text = valueAnimator.animatedValue.toString()
+        val animator = ValueAnimator.ofInt(0, 10)
+
+        animator.duration = 10000
+        animator.addUpdateListener { valueAnimator ->
+
+            binding.textViewValueAnimator.text = valueAnimator.animatedValue.toString()
+        }
+
+        binding.buttonStartValueAnimation.setOnClickListener {
+            animator.start()
+        }
+
+        binding.buttonEndValueAnimator.setOnClickListener {
+            animator.end()
+        }
+
+        binding.buttonCancelValueAnimator.setOnClickListener {
+            animator.cancel()
+        }
     }
-
-    buttonStartValueAnimation.setOnClickListener {
-      animator.start()
-    }
-
-    buttonEndValueAnimator.setOnClickListener {
-      animator.end()
-    }
-
-    buttonCancelValueAnimator.setOnClickListener {
-      animator.cancel()
-    }
-
-  }
 }
